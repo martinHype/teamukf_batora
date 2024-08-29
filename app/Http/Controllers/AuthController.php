1039d8b71e4bd7b6183212727b8f3b175579c113
martinHype <batora.martin33@gmail.com>
2024-08-29 18:29:06 +0200

    adding the registration functionality to app

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request){
+        $fields = $request->validate([
+            'name' => 'required:max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6|confirmed',
+        ]);
+
+        $created_user = User::create($fields);
+
+        $token = $created_user->createToken($request->name);
+
+        return [
+            'user' => $created_user,
+            'token' => $token->plainTextToken
+        ];
+    }
+
+    public function login(Request $request){
+        $request->validate([
+            'email' => 'required|email|exists:users',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('email',$request->email)->first();
+        if(!$user || !Hash::check($request->password, $user->password)){
+            return ["message"=>"The provided credentials are incorrect."];
+        }
+        $token = $user->createToken($user->name);
+
+        return [
+            'user' => $user,
+            'token' => $token->plainTextToken
+        ];
+
+
+
+    }
+
+    public function logout(Request $request){
+        return "logout";
+    }
+}
